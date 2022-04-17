a8a654ff99b2cefab5e87ab7ab245820a28d7031
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-04-17 10:15:05 +0200

    funcion run

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 import socket
 import threading
+
 class hilo_servidor(threading.Thread):
     def __init__(self,conexion,direccion, sockets):
         threading.Thread.__init__(self)
         self.conexion = conexion
         self.direccion = direccion
+    def run(self):
+        print("\nNew conexion : ", self.direccion[0])
+        while True:
+            data = self.conexion.recv(2048)
+            dt = data.decode()
+            if dt == '':
+                continue
+            print(self.direccion[0], " > ", dt)
 class servidor():
     host = socket.gethostname()
     port = 12345
