25f6d3936ef39fa0725b4ce1e65b4ed8b26a772b
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-04-17 10:38:16 +0200

    mismo proceso con cliente

--- client.py
+++ client.py
@@ -1,13 +1,24 @@
+from concurrent.futures import thread
 import socket
+import threading
 
-host = socket.gethostname()
-port = 12345
+class hilo_cliente(threading.Thread):
+    def __init__ (self, socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_cliente:
-    print("Socket establecido")
-    socket_cliente.connect((host, port))
-    print("Socket conectado")
-    while True:
-        data = input('Escribe algo > ')
-        dt = bytes(data, 'UTF-8')
-        socket_cliente.send(dt)
+
+
+class cliente():
+    def iniciar():
+        host = socket.gethostname()
+        port = 12345
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_cliente:
+            print("Socket establecido")
+            socket_cliente.connect((host, port))
+            print("Socket conectado")
+            while True:
+                data = input('Escribe algo > ')
+                dt = bytes(data, 'UTF-8')
+                socket_cliente.send(dt)
