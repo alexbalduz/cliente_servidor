e6efb025798a30bbe5fb1ce2e3c99558de8a9bbb
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-04-17 10:06:56 +0200

    comprobacion funcionamiento server y acepta conexiones

--- server.py
+++ server.py
@@ -6,3 +6,9 @@ port = 12345
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_server:
     socket_server.bind((host, port))
     socket_server.listen(5)
+
+    print("\nSocket establecido")
+
+    conexion_socket, direccion = socket_server.accept()
+
+    print("Primera conexion : ", direccion[0])
